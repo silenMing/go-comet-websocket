7a6a29f1282921d349ccbbb0d880647b56473fbd
zhangming <zhangming@shopex.cn>
2017-11-21 23:59:18 +0800

    create server

--- main.go
+++ main.go
@@ -2,5 +2,21 @@ package main
 
 import (
 	"net/http"
+  "log"
 	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+    ReadBufferSize:  1024,
+    WriteBufferSize: 1024,
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+    ws, err := upgrader.Upgrade(w, r, nil)
+
+    if err != nil {
+        log.Println(err)
+        return
+    }
+
+}
