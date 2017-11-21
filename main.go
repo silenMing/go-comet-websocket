7d81e63f1f8074b76107a0ba796deef151c0a16f
zhangming <zhangming@shopex.cn>
2017-11-22 00:16:13 +0800

    read/send

--- main.go
+++ main.go
@@ -11,6 +11,10 @@ var upgrader = websocket.Upgrader{
     WriteBufferSize: 1024,
 }
 
+type connection struct{
+  ws *websocket.Coon
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
     ws, err := upgrader.Upgrade(w, r, nil)
 
@@ -19,4 +23,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
         return
     }
 
+}
+
+func (c *connection) writer() {
+    for message := range c.send {
+        err := c.ws.WriteMessage(websocket.TextMessage, message)
+        if err != nil {
+            break
+        }
+    }
+    c.ws.Close()
+}
+
+func (c *connection) reader() {
+    for {
+      _,message,err := c.ws.ReadMessage()
+      if err != nil {
+        log.Println(err)
+        break
+      }
+      h.broadcast <- message
+    }
+    c.ws.Close()
 }
