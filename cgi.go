cb5e44ab929f6ddb5656a25532c831d3198c13c7
zhangming <zhangming@shopex.com>
2017-12-21 23:54:12 +0800

    cgi

--- cgi.go
+++ cgi.go
@@ -9,8 +9,16 @@ import(
 func main(){
 	http.HandleFunc("/",func(w http.ResponseWriter,r *http.Request){
 		handler := new(cgi.Handler);
-	})
-		
+		handler.Path = "/usr/local/go/bin/go";
+		script := "$HOME/cgi-script" + r.URL.Path;
+		fmt.Println(handler.Path);
+		args := []string{"run",script}
+		handler.Args = append(handler.Args, args...);
+
+		handler.ServeHTTP(w, r);
+	});
+	http.ListenAndServe(":8899",nil)
+	select {}//阻塞进程
 	
 }
 
