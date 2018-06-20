20ca5f7465b32e0595b6b75b912c72785388bc44
zhangming <zhangming@shopex.com>
2018-06-20 15:08:52 +0800

    fix server

---
+++ server.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+
+	//建立socket
+	netListen, err := net.Listen("tcp", "127.0.0.1:9001")
+	CheckError(err)
+	defer netListen.Close()
+
+	Log("wait client")
+
+	for {
+		conn, err := netListen.Accept()
+
+		if err != nil {
+			continue
+		}
+		Log(conn.RemoteAddr().String(), " connect success")
+
+		handleConnect(conn)
+	}
+}
+
+// 处理链接
+
+func handleConnect(conn net.Conn) {
+	buffer := make([]byte, 2048)
+
+	for {
+		n, err := conn.Read(buffer)
+
+		if err != nil {
+			Log(conn.RemoteAddr().String(), " connection error: ", err)
+			return
+		}
+
+		Log(conn.RemoteAddr().String(), "receive data string:\n", string(buffer[:n]))
+
+	}
+}
+
+func CheckError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %s", err.Error())
+		os.Exit(1)
+	}
+}
+
+func Log(v ...interface{}) {
+	log.Println(v...)
+}
